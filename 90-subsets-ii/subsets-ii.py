a4af2f01dca6826cb269bb16f2a9e379a8a02c78
Tech-anurag95 <dubeyanurag7991@gmail.com>
2026-09-06 21:59:31 +0530

    Time: 7 ms (5.86%) | Memory: 19.4 MB (60.76%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -5,11 +5,14 @@ class Solution:
 
         for num in nums:
             curr = []
+
             for subset in out:
                 curr.append(subset + [num])
+
             out += curr
 
         ans = []
+
         for x in out:
             if x not in ans:
                 ans.append(x)
