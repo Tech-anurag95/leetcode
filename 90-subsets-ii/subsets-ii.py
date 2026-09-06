189854ef0cec57e2662ab4a33016a25d3fee0c7b
Tech-anurag95 <dubeyanurag7991@gmail.com>
2026-09-06 21:40:35 +0530

    Time: 3 ms (21.73%) | Memory: 19.4 MB (60.76%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        out = [[]]
+
+        for num in nums:
+            curr = []
+            for subset in out:
+                curr.append(subset + [num])
+            out += curr
+
+        ans = []
+        for x in out:
+            if x not in ans:
+                ans.append(x)
+
+        return ans
