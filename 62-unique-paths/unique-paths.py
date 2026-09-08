57f1317c97e292b1e494e701a7ebef564b95afaf
Tech-anurag95 <dubeyanurag7991@gmail.com>
2026-09-08 23:00:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (22.56%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -9,3 +9,13 @@ class Solution:
             for j in range(1,n):
                 dp[i][j]=dp[i-1][j]+dp[i][j-1]
         return dp[m-1][n-1]
+
+
+
+        #  dp = [[1]*n for _ in range(m)]
+        #  for i in range(1,m):
+        #     for j in range(1,n):
+        #         dp[i][j]=dp[i-1][j]+dp[i][j-1]
+        # return dp[m-1][n-1]
+
+        # we can skill filling first row and column element 1 if we initialize matrix with 1 as its elements instead of 0
