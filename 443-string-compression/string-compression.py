0cdde49f16cae1935b77509b4b5d861643f89d7e
Anurag Dubey <dubeyanurag7991@gmail.com>
2026-09-17 19:49:24 +0530

    Time: 3 ms (44.30%) | Memory: 19.3 MB (88.06%) - LeetSync

---
+++ 443-string-compression/string-compression.py
@@ -0,0 +1,19 @@
+class Solution:
+    def compress(self, chars: list[str]) -> int:
+        a = []
+        count = 1
+        n = len(chars)
+
+        for i in range(n):
+            if i + 1 < n and chars[i] == chars[i + 1]:
+                count += 1
+            else:
+                a.append(chars[i])
+
+                if count > 1:
+                    a.extend(str(count))
+
+                count = 1
+
+        chars[:] = a
+        return len(a)
