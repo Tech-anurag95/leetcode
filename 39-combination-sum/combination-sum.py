f5aff66efeec27e35a7b057aa8a5c4b10cd35cb1
Anurag Dubey <dubeyanurag7991@gmail.com>
2026-09-19 16:58:42 +0530

    Time: 13 ms (25.48%) | Memory: 19.4 MB (91.85%) - LeetSync

---
+++ 39-combination-sum/combination-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        ans = []
+        stack = [(0, [])]
+
+        while stack:
+            start, path = stack.pop()
+
+            total = sum(path)
+
+            if total == target:
+                ans.append(path)
+                continue
+
+            if total > target:
+                continue
+
+            for i in range(start, len(candidates)):
+                stack.append((i, path + [candidates[i]]))
+
+        return ans
