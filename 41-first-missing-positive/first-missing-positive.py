dd002d49e2c00cf06b4b9aa276088c3951d086c3
Anurag Dubey <dubeyanurag7991@gmail.com>
2026-09-20 13:26:05 +0530

    Time: 21 ms (85.74%) | Memory: 34.9 MB (13.60%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,7 +1,7 @@
 class Solution:
     def firstMissingPositive(self,nums):
-        s=set(nums)
-        i=1
+        s=set(nums) #too remove the duplicates and checking same number many times
+        i=1  #start of positive integer
         while i in s:
             i+=1
         return i
