3dd937f4d845c0c363973cda0f4ffd0155b95219
Anurag Dubey <dubeyanurag7991@gmail.com>
2026-09-23 13:17:09 +0530

    Time: 11 ms (86.85%) | Memory: 22 MB (28.27%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.py
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.py
@@ -4,20 +4,15 @@ class Solution:
         for i in range(len(customers)):
             if grumpy[i]==0:
                 alreadysatisfied+=customers[i]
-
         window=0
         ans=0
         left=0
-
         for right in range(len(customers)):
             if grumpy[right]==1:
                 window+=customers[right]
-
             if right-left+1>minutes:
                 if grumpy[left]==1:
                     window-=customers[left]
                 left+=1
-
             ans=max(ans,window)
-
         return alreadysatisfied+ans
