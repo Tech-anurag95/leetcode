b630a1751f15f529d5c59d203e675cb491c3ddf4
Anurag Dubey <dubeyanurag7991@gmail.com>
2026-09-25 11:46:42 +0530

    Time: 105 ms (43.86%) | Memory: 19.8 MB (16.66%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -6,15 +6,11 @@ class Solution:
         count=Counter()
         max_freq=0
         bda=0
-
-        for right in range(len(s)):
-            count[s[right]]+=1
-            max_freq=max(max_freq,count[s[right]])
-
-            while right-left+1-max_freq>k:
-                count[s[left]]-=1
-                left+=1
-
-            bda=max(bda,right-left+1)
-
+        for right in range(len(s)):    #saare element ko consider krenge 
+            count[s[right]]+=1         #jaise hi wo element window me ghusa usi frequency ko 1 bdha denge 
+            max_freq=max(max_freq,count[s[right]])   #current max frequency ko consider krenge 
+            while right-left+1-max_freq>k:   #jitne element change kr skte hain wo agar k se bda ho jayega to humko windoiw size chota krna pdega n 
+                count[s[left]]-=1  #jo element nikalenge uski count ko 1 kam kr denge 
+                left+=1   #window ko choti kr denge 
+            bda=max(bda,right-left+1)  #valid max length window ko consider krenge 
         return bda
